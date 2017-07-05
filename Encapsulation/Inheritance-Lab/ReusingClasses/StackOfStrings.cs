f3b9b64ffa294998b00c2ccd4d044c40b9b0444a
szdavidkov <stanislav.davidkov1@gmail.com>
2017-07-05 15:12:13 +0300

    Inheritance Lab Exercises

---
+++ Encapsulation/Inheritance-Lab/ReusingClasses/StackOfStrings.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+public class StackOfStrings
+{
+    private List<string> data;
+
+    public List<string> Data
+    {
+        get { return this.data; }
+        set { this.data = value; }
+    }
+
+    public void Push(string item)
+    {
+        
+    }
+
+    public string Pop()
+    {
+        return "";
+    }
+
+    public string Peek()
+    {
+        return "";
+    }
+
+    public bool IsEmpty()
+    {
+        return true;
+    }
+}
+
